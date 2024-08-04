e44bd56cc95f0143809ae945d6758c23541fc690
Metanon Jongkraijak <imetanon@gmail.com>
2024-08-05 00:44:56 +0700

    fix lib issues

--- install_glibc.sh
+++ install_glibc.sh
@@ -4,6 +4,17 @@
 apt-get update && \
     apt-get install -y wget build-essential python3 python3-pip
 
+# Download and install the latest version of SQLite (3.35.0 or higher)
+SQLITE_VERSION=3.37.2
+wget https://www.sqlite.org/2022/sqlite-autoconf-${SQLITE_VERSION}000.tar.gz && \
+    tar -xzf sqlite-autoconf-${SQLITE_VERSION}000.tar.gz && \
+    cd sqlite-autoconf-${SQLITE_VERSION}000 && \
+    ./configure --prefix=/usr/local && \
+    make -j$(nproc) && \
+    make install && \
+    cd .. && \
+    rm -rf sqlite-autoconf-${SQLITE_VERSION}000 sqlite-autoconf-${SQLITE_VERSION}000.tar.gz
+
 # Download glibc 2.29
 wget http://ftp.gnu.org/gnu/libc/glibc-2.29.tar.gz && \
     tar -xzf glibc-2.29.tar.gz
@@ -21,7 +32,7 @@ cd ../.. && \
 
 # Export environment variables
 echo 'export LD_LIBRARY_PATH=/usr/local/glibc-2.29/lib:$LD_LIBRARY_PATH' >> ~/.bashrc
-echo 'export PATH=/usr/local/glibc-2.29/bin:$PATH' >> ~/.bashrc
+echo 'export PATH=/usr/local/glibc-2.29/bin:/usr/local/bin:$PATH' >> ~/.bashrc
 source ~/.bashrc
 
 # Install Python dependencies
