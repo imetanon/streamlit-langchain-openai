034220755a04603d09c7998bff573311ee3c9c89
Metanon Jongkraijak <imetanon@gmail.com>
2024-08-05 00:58:06 +0700

    fix lib issues

--- start.sh
+++ start.sh
@@ -4,5 +4,8 @@
 export LD_LIBRARY_PATH=/usr/local/glibc-2.29/lib:$LD_LIBRARY_PATH
 export PATH=/usr/local/glibc-2.29/bin:/usr/local/bin:$PATH
 
+# Activate the virtual environment
+source venv/bin/activate
+
 # Start the Streamlit app
 streamlit run Chatbot.py --server.port 8080
